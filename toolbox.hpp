d36d7d8b754d71e2c5dc5a2e5d0b6c89a2e6e6f1
Ted Middleton <middleton.ted@gmail.com>
2024-02-11 15:09:31 -0800

    Adding the timeit function

---
+++ toolbox.hpp
@@ -0,0 +1,35 @@
+#ifndef INCLUDED_TOOLBOX_HPP
+#define INCLUDED_TOOLBOX_HPP
+
+#include <intrin.h>
+
+namespace tb
+{
+
+template<typename... Args1, typename... Args2>
+void timeit(uint64_t& elapsed, void(*func)(Args1...), Args2&& ...args)
+{
+    constexpr int NUM_ITERATIONS = 1;
+    uint64_t begin = __rdtsc();
+    for (int i = 0; i < NUM_ITERATIONS; ++i)
+    {
+        func(args...);
+    }
+    elapsed = (__rdtsc() - begin) / NUM_ITERATIONS;
+}
+
+template<typename... Args1, typename... Args2>
+void timeit(size_t iters, uint64_t& elapsed, void(*func)(Args1...), Args2&& ...args)
+{
+    size_t NUM_ITERATIONS = iters;
+    uint64_t begin = __rdtsc();
+    for (size_t i = 0; i < NUM_ITERATIONS; ++i)
+    {
+        func(args...);
+    }
+    elapsed = (__rdtsc() - begin) / NUM_ITERATIONS;
+}
+
+} // namespace tb
+
+#endif // INCLUDED_TOOLBOX_HPP
