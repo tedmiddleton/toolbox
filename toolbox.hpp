01614100821fae592aaaecdf7745b4052552a984
Ted Middleton <middleton.ted@gmail.com>
2024-02-13 17:05:59 -0800

    Modified timeit to properly forward template arguments

--- toolbox.hpp
+++ toolbox.hpp
@@ -2,32 +2,35 @@
 #define INCLUDED_TOOLBOX_HPP
 
 #include <intrin.h>
+#include <cstdint>
 
 namespace toolbox
 {
 
-template<typename... Args1, typename... Args2>
-void timeit(uint64_t& elapsed, void(*func)(Args1...), Args2&& ...args)
+template<typename Func, typename... Args>
+uint64_t timecycles(Func func, Args&& ...args)
 {
-    constexpr int NUM_ITERATIONS = 1;
-    uint64_t begin = __rdtsc();
-    for (int i = 0; i < NUM_ITERATIONS; ++i)
-    {
-        func(args...);
-    }
-    elapsed = (__rdtsc() - begin) / NUM_ITERATIONS;
-}
-
-template<typename... Args1, typename... Args2>
-void timeit(size_t iters, uint64_t& elapsed, void(*func)(Args1...), Args2&& ...args)
-{
-    size_t NUM_ITERATIONS = iters;
+    constexpr size_t NUM_ITERATIONS = 10;
     uint64_t begin = __rdtsc();
     for (size_t i = 0; i < NUM_ITERATIONS; ++i)
     {
-        func(args...);
+        func(std::forward<Args>(args)...);
     }
-    elapsed = (__rdtsc() - begin) / NUM_ITERATIONS;
+    uint64_t elapsed = (__rdtsc() - begin) / NUM_ITERATIONS;
+    return elapsed;
+}
+
+template<typename Func, typename... Args>
+uint64_t timecycles(const size_t iterations, Func func, Args&& ...args)
+{
+    const size_t NUM_ITERATIONS = iterations;
+    uint64_t begin = __rdtsc();
+    for (size_t i = 0; i < NUM_ITERATIONS; ++i)
+    {
+        func(std::forward<Args>(args)...);
+    }
+    uint64_t elapsed = (__rdtsc() - begin) / NUM_ITERATIONS;
+    return elapsed;
 }
 
 } // namespace toolbox
